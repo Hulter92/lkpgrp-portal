ea3a37f346f58d3cdd596925e194a4a509bcef96
David Hulterström <david.hulterstrom@gmail.com>
2026-05-06 21:52:12 +0200

    add role to token

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,13 +9,39 @@ export const authOptions = {
     }),
   ],
   callbacks: {
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub;
+  async jwt({ token }) {
+    if (!token.sub) return token;
+
+    try {
+      const res = await fetch(
+        `${process.env.NEXTAUTH_URL}/api/discord/role?userId=${token.sub}`
+      );
+
+      const data = await res.json();
+      const roles = data.roles || [];
+
+      if (roles.includes("1501163659760500868")) {
+        token.role = "Headquarters";
+      } else if (roles.includes("1501165906347425872")) {
+        token.role = "Whitelisted";
+      } else {
+        token.role = "Ansökande";
       }
-      return session;
-    },
+    } catch {
+      token.role = "Ansökande";
+    }
+
+    return token;
   },
+
+  async session({ session, token }) {
+    if (session.user) {
+      session.user.id = token.sub;
+      session.user.role = token.role;
+    }
+    return session;
+  },
+}
 };
 
 const handler = NextAuth(authOptions);
